1904c4a9eaefd4da7a53cb10287ed4397539a759
dsh <ddy_dhj@163.com>
2018-06-21 11:43:29 +0800

    配置小程序，寻找接口

--- config.js
+++ config.js
@@ -2,39 +2,28 @@
  * 小程序配置文件
  */
 
-// 此处主机域名是腾讯云解决方案分配的域名
-// 小程序后台服务解决方案：https://www.qcloud.com/solution/la
+// 定义API服务器，图片服务器，主域名
+var host = "https://mainsite-restapi.ele.me"
+var imgHost = "https://fuss10.elemecdn.com"
+var domain = "https://www.ele.me"
 
-var host = "14592619.qcloud.la"
+// 配置纬度，经度
+var latitude = 34.749779
+var longitude = 113.651146
 
 var config = {
-
-  // 下面的地址配合云端 Server 工作
   host,
+  imgHost,
+  domain,
 
-  // 登录地址，用于建立会话
-  loginUrl: `https://${host}/login`,
+  latitude,
+  longitude,
 
-  // 测试的请求地址，用于测试会话
-  requestUrl: `https://${host}/testRequest`,
-
-  // 用code换取openId
-  openIdUrl: `https://${host}/openid`,
-
-  // 测试的信道服务接口
-  tunnelUrl: `https://${host}/tunnel`,
-
-  // 生成支付订单的接口
-  paymentUrl: `https://${host}/payment`,
-
-  // 发送模板消息接口
-  templateMessageUrl: `https://${host}/templateMessage`,
-
-  // 上传文件接口
-  uploadFileUrl: `https://${host}/upload`,
-
-  // 下载示例图片接口
-  downloadExampleUrl: `https://${host}/static/weapp.jpg`
+  // 以下是要访问的各种接口：
+  geo: `${host}/bgs/poi/reverse_geo_coding`,
+  entries: `${host}/shopping/openapi/entries`,
+  restaurants: `${host}/shopping/v3/restaurants`
+  
 };
 
 module.exports = config
